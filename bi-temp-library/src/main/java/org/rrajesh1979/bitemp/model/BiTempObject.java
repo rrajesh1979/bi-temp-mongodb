8e40041f9a3712e49311a2f9fda94b917d90997f
rrajesh1979 <rrajesh1979@gmail.com>
2022-12-24 19:09:56 -0500

    ✅Adding _id field in BiTempObject - could be useful for updates

--- bi-temp-library/src/main/java/org/rrajesh1979/bitemp/model/BiTempObject.java
+++ bi-temp-library/src/main/java/org/rrajesh1979/bitemp/model/BiTempObject.java
@@ -2,15 +2,19 @@ package org.rrajesh1979.bitemp.model;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import org.bson.types.ObjectId;
 import org.rrajesh1979.bitemp.utils.DateTimeSerializer;
 
 import java.time.OffsetDateTime;
 
-public record BiTempObject(Object id, Object data, RecordMeta recordMeta, EffectiveMeta effectiveMeta,
-                           Object previousId, Object nextId) {
+public record BiTempObject(
+        Object key,
+        Object data,
+        RecordMeta recordMeta,
+        EffectiveMeta effectiveMeta,
+        Object previousId,
+        Object nextId,
+        ObjectId _id) {
     //Override toString() method to print the object in JSON format
     @Override
     public String toString() {
