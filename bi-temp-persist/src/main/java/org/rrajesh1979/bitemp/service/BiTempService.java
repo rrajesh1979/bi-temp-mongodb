785fcd378bb3474ba31a8f22f8aef07df9d7c4b4
rrajesh1979 <rrajesh1979@gmail.com>
2022-12-24 19:10:26 -0500

    ✅Adding _id field in BiTempObject - could be useful for updates

--- bi-temp-persist/src/main/java/org/rrajesh1979/bitemp/service/BiTempService.java
+++ bi-temp-persist/src/main/java/org/rrajesh1979/bitemp/service/BiTempService.java
@@ -146,6 +146,7 @@ public class BiTempService {
                 recordMeta,
                 effectiveMeta,
                 null,
+                null,
                 null);
         log.debug("BiTemp Object: {}", biTempObject);
         return biTempObject;
