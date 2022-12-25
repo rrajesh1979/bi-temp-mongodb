e2cefd6a1d54c71ffcff02d13c1d0566a671ea1c
rrajesh1979 <rrajesh1979@gmail.com>
2022-12-24 20:45:02 -0500

    ♻️Refactoring code - updating key name

--- bi-temp-persist/src/main/java/org/rrajesh1979/bitemp/model/CreateRequest.java
+++ bi-temp-persist/src/main/java/org/rrajesh1979/bitemp/model/CreateRequest.java
@@ -3,7 +3,7 @@ package org.rrajesh1979.bitemp.model;
 import java.time.LocalDateTime;
 
 public record CreateRequest(
-        Object id,
+        Object key,
         Object data,
         String createdBy,
         LocalDateTime effectiveFrom,
